3321ec13336bff76e6e00ab65677ff5a49daf39c
connectednode <>
2021-02-15 10:11:33 +0100

    added two ways to use the directinal keys

---
+++ directinal1.sh
@@ -0,0 +1,6 @@
+# diractinol keys setings for mofing trouw windows
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 8 "key +super up -super"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 9 "key +super down -super"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 11 "key +super left -super"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 12 "key +super right -super"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 10 "key +super +ctrl 2 -ctrl -super"
---
+++ directinal2.sh
@@ -0,0 +1,6 @@
+# diractinol keys setings as arow keys
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 8 "key +up"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 9 "key +down"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 11 "key +left"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 12 "key +right"
+xsetwacom set "HUION Huion Tablet_H320M Pad pad" Button 10 "key +super +ctrl 1 -ctrl -super"
