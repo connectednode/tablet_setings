3ddddaec08add5e338554f9bd881d52f50262bd3
connectednode <>
2021-04-26 18:46:40 +0200

    some config files

--- osu.sh
+++ osu.sh
@@ -1,3 +1,4 @@
+/home/blackbox/bin/tablet_conf/tablet.sh
 xsetwacom set "HUION Huion Tablet_H320M stylus" mode Absolute
 xsetwacom set "HUION Huion Tablet_H320M stylus" Area 0 0 45720 25718
 xsetwacom set "HUION Huion Tablet_H320M stylus" MapToOutput HDMI-1
